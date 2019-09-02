caf49153f604a4098fbf2aa652973e8f51acc672
ikhide <ikhide.atakpu@gmail.com>
2019-09-02 14:00:57 +0100

    firebase setup update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
   UNSAFE_componentWillMount() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyDmW6rdTWFMlBg1LB4w3MSIo8917uYlp0o',
-      authDomain: 'authentication-e60d4.web.com',
+    const    firebaseConfig = {
+      apiKey: "AIzaSyDmW6rdTWFMlBg1LB4w3MSIo8917uYlp0o",
+      authDomain: "authentication-e60d4.firebaseapp.com",
+      databaseURL: "https://authentication-e60d4.firebaseio.com",
+      projectId: "authentication-e60d4",
+      storageBucket: "",
+      messagingSenderId: "379111771984",
+      appId: "1:379111771984:web:7177ce40161a7a8c"
     };
+    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
   }
 
