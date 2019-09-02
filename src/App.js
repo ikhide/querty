6b0b2f775a9893b777fb1da3d04d3deb1451ad2c
Kovic Victor <ikhide.atakpu@gmail.com>
2019-09-02 11:14:10 +0100

    rearrange import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
-import firebase from 'firebase'
 
 export default class App extends Component {
-  componentWillMount(){
-    const firebaseConfig={
-      apiKey:'AIzaSyDmW6rdTWFMlBg1LB4w3MSIo8917uYlp0o',
-      authDomain:'authentication-e60d4.firebaseapp.com'
-    }
-    firebase.initializeApp(firebaseConfig)
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDmW6rdTWFMlBg1LB4w3MSIo8917uYlp0o',
+      authDomain: 'authentication-e60d4.firebaseapp.com',
+    };
+    firebase.initializeApp(firebaseConfig);
   }
 
   render() {
